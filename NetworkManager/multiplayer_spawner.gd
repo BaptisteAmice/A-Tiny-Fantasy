a6ecf9814913e203a699170289d0c21954739414
baptiste amice <amice.bap@gmail.com>
2025-10-17 22:51:52 +0200

    patch: other clients where trying to spawn the player, now only the server can try

--- NetworkManager/multiplayer_spawner.gd
+++ NetworkManager/multiplayer_spawner.gd
@@ -10,13 +10,18 @@ func _ready() -> void:
 
 @rpc("any_peer", "call_remote", "reliable")
 func request_spawn(selected_name: String) -> void:
+	if not multiplayer.is_server(): return
 	# This function is called *on the server* by the client
 	var peer_id: int = multiplayer.get_remote_sender_id()
 	print("Client", peer_id, "requested spawn of character", selected_name)
 	var spawned_player: Player =  spawn_player(peer_id, selected_name)
+
+	if spawned_player == null:
+		push_error("spawn_player returned null!")
+		return
 	
 	# Make sure the node is in the tree before sending the path of the instantiated player
-	call_deferred("_send_spawned_player_path", peer_id, spawned_player)
+	_send_spawned_player_path(peer_id, spawned_player)
 
 # validate spawn to client
 func _send_spawned_player_path(peer_id: int, player: Player) -> void:
@@ -32,12 +37,16 @@ func receive_spawned_player(player_path: NodePath) -> void :
 	
 func spawn_player(peer_id: int, selected_name: String) -> Player:
 	print("trying to spawn")
-	if !multiplayer.is_server(): return
+	if !multiplayer.is_server(): 
+		push_error("Only the server should call the function spawn_player")
+		return
 	var player: Player = network_player.instantiate()
 	player.name = str(peer_id) # crucial
 	# Restore data if saved
+	print("selected name: " + selected_name)
 	var saved_state: Variant = Global.game_controller.network_manager.registered_players.get(selected_name, null)
 	player.load(saved_state)
-	get_node(spawn_path).call_deferred("add_child", player)
+	var parent_node: Node = get_node(spawn_path)
+	parent_node.add_child(player)
 	print("character spawned")
 	return player
