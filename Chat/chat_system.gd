fb7f90821afa7624358de09ea1465d2188d83ac2
baptiste amice <amice.bap@gmail.com>
2026-02-22 03:20:01 +0100

    add the player's name to the message

--- Chat/chat_system.gd
+++ Chat/chat_system.gd
@@ -3,16 +3,19 @@ class_name ChatSystem
 
 func send_message_from_client(message: String) -> void:
 	if multiplayer.is_server(): return
-	if message.strip_edges().is_empty():
+	message = message.strip_edges()
+	if message.is_empty():
 		return
 	if message.begins_with("/"):
 		process_command_on_client_from_message(message)
 	else:
+		# add the player's name to the message
+		message = PlayerManager.my_player.name_label.text + "> " + message
 		#send message to server
 		rpc_id(1, "_server_receive_message", message)
 
 func process_command_on_client_from_message(message: String) -> void:
-	if multiplayer.is_server(): return 
+	if multiplayer.is_server(): return
 	Global.game_controller.signals_bus.ADD_MESSAGE_TO_DISPLAY_LIST.emit(message)
 	#todo
 	pass
